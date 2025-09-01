5e63ac90046b125ca8d22dca0ea7ec1f435bdc2a
ruck94301 <John.Ruckstuhl@gmail.com>
2025-09-01 15:57:35 -0700

    Move pyinstaller files to separate subdir.

---
+++ LabGym/pyinstaller/pyinstaller.sh
@@ -0,0 +1,11 @@
+set -x
+
+# pyinstaller --add-data=logging.yaml:LabGym --noconfirm app.py 
+pyinstaller --add-data=../logging.yaml:LabGym --noconfirm app.py 
+
+wc -l build/app/warn-app.txt
+
+(
+    date
+    dist/app/app --debug
+)
